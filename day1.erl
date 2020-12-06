0d466a3cac005666ea2994c1211f8a5db3d89ef9
raerkeer <rainer@mfge.ch>
2020-12-06 21:00:37 +0100

    day1 bruteforce update

--- day1.erl
+++ day1.erl
@@ -1,14 +1,21 @@
 -module(day1).
 -export([run/0]).
 
+% A simple "brute force" solution
+
 run()->
-    process_list(load_file("day1input.txt")).
+    Numbers = load_file("day1input.txt"),
+    {part1(Numbers), part2(Numbers)}.
 
 load_file(Filename)->
     {ok, Binary} = file:read_file(Filename),
     StringContent = unicode:characters_to_list(Binary),
     [ element(1, string:to_integer(Substr)) || Substr <- string:tokens(StringContent, "\n")]. 
 
-process_list(L)->
-    [{X, Y, X*Y} || X <- L, Y <- L, X + Y =:= 2020 ].
+part1(L)->
+    [H | _] = [{X, Y, X*Y} || X <- L, Y <- L, X + Y =:= 2020],
+    H.
 
+part2(L)->
+    [H | _] = [{X, Y, Z, X*Y*Z} || X <- L, Y <- L, Z <- L, X + Y + Z =:= 2020],
+    H.
