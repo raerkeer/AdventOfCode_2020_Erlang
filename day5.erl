ed353fa307905c0ebc8386b56ad1d364cca0f2b7
raerkeer <rainer@mfge.ch>
2020-12-07 07:14:47 +0100

    day5

---
+++ day5.erl
@@ -0,0 +1,36 @@
+-module(day5).
+-export([run/0]).
+
+run()->
+    SeatCodes = load_file("day5input.txt"),
+    SeatIds = lists:map(fun(SeatCode) -> calc_seatid(SeatCode) end, SeatCodes),
+    {part1(SeatIds), part2(SeatIds)}.
+
+part1(SeatIds)->
+    lists:max(SeatIds).
+
+part2(SeatIds)->
+    SortedIds = lists:sort(SeatIds),
+    my_seat(SortedIds).
+
+my_seat([X, Y | T])->
+    case Y-X of
+        2 -> X+1;
+        1 -> my_seat([Y|T])
+    end.
+
+calc_seatid(SeatCode)->
+    Bits = lists:map(fun(X) -> get_bit(X) end, SeatCode),
+    Row = list_to_integer(string:slice(Bits, 0, 7), 2),
+    Col = list_to_integer(string:slice(Bits, 7, 3), 2),
+    Row * 8 + Col.
+
+get_bit($F) -> $0;
+get_bit($B) -> $1;
+get_bit($L) -> $0;
+get_bit($R) -> $1.
+
+load_file(Filename)->
+    {ok, Binary} = file:read_file(Filename),
+    StringContent = unicode:characters_to_list(Binary),
+    [ Line || Line <- string:tokens(StringContent, "\n")]. 
